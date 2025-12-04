3142bf66c9bc4aeb9e12e3524558550d464e8238
Jordan Malek <jordan.malek@skillsoftware.com>
2025-12-04 14:26:19 +0100

    Fixed solution for day 3 and created gitignore.

--- day3/code.c
+++ day3/code.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <deque>
 int64_t problemOne(FILE *inputFile){
     #define bufferSize 4196
     char inputChars[bufferSize];
@@ -35,29 +34,33 @@ int64_t problemTwo(FILE *inputFile){
     int64_t total = 0;
     char *bufStart = &inputChars[0];
     while(fgets(bufStart,bufferSize,inputFile)){
-        std::deque<int> maxes;
-        int left = -1;
+        int64_t maxes[12] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
         char c;
         for(char *p = bufStart;(c = *p) >= '0' && c <= '9';++p)
         {
             int digit = (c - '0');
-            if(maxes.size() >= 11)
-            {
-                int64_t newCandidate = maxes[10] + digit;
-                if(maxes[11] < newCandidate)
-                    maxes[11] = newCandidate;
+            for(int64_t* p = &maxes[11]; p >= &maxes[0]; --p){
+                int64_t cur = *p;
+                int64_t candidate = -1;
+                int64_t prev;
+                if(p == &maxes[0])
+                    candidate = digit;
+                else if((prev = *(int64_t*)(p - 1)) >= 0)
+                    candidate = (prev * 10) + digit;
+
+                if(candidate > cur){
+                    *p = candidate;
+                }
             }
-            if(digit > left)
-                left = digit;
-            
         }
-        total += maxes[11];
+        if(maxes[11] > 0)
+            total += maxes[11];
     }
     return total;
 }
 int main(int argc, char **argv){
     FILE *inputFile = fopen("input.txt", "r");
-    int64_t result = problemOne(inputFile);
+    int64_t result = problemTwo(inputFile);
     printf("%ld\n", result);
     fclose(inputFile);
 }
